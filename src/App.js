87c5a7eacd8612c28f6e98578d6baf6ace4573d3
Taras Shulhan <taras.shulhan@gmail.com>
2021-11-12 01:17:35 +0200

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import axios from 'axios';
+import Picture from './Picture';
 
 function App() {
-  const [date, setDate] = useState(null);
+
+  let [astroImg, setAstroImg] = useState(null);
+
   useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+    axios.get('https://random-data-api.com/api/crypto_coin/random_crypto_coin')
+      .then(res => {
+        setAstroImg(res.data)
+      }).catch(err => {
+        console.error(err);
+      })
+  },[])
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <div className="App">
+      {astroImg && <Picture picUrl={astroImg.logo} picTitle={astroImg.acronym} picDesc={astroImg.coin_name}/>}
+    </div>
   );
 }
 
